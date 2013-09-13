1bc3fd94c0f558cf1d8e04b85a181b28e389ce08
tzachi <tzachirtuv@gmail.com>
2013-09-13 15:24:10 +0300

    simplified the User controll

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HiProjecy::Application.config.secret_key_base = '929cd9dbedac6c87855e65b8e6f9b3c00a48c6d2f1671dba6f080b703352f5af410f94c9c44820230d00ad91aaaa3267048b99fe253de6b116060489e825b6c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HiProjecy::Application.config.session_store :cookie_store, key: '_hiProjecy_session'
